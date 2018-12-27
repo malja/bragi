41a4c294305e8deaa34ed1170e21c88af4cd3275
malja <github@malcak.cz>
2018-12-27 01:15:32 +0100

    Detector class moved to components directory

--- bragi/components/detector.py
+++ bragi/components/detector.py
@@ -1,8 +1,5 @@
 import cv2
-import os
 import numpy as np
-import argparse
-import uuid
 import time
 import datetime
 import math
@@ -10,22 +7,45 @@ import math
 from bragi import Config
 
 class Detector:
+    """
+    Detect faces in given video. 
+
+    It goes through the video frame by frame (or skips every `config.detection.skip_frames`) and detect all faces.
+    Classifiers defined in `config.detection.classifiers` are used. When any number of faces is detected in frame,
+    array containing all of them is yield-ed from `detect` method.
+
+    Usage:
+    ======
+
+        with Detector("path_to_video", config_object) as d:
+            for faces in d.detect():
+                for face in faces:
+                    # ... do something with face
+
+    """
+
     def __init__(self, input_path: str, config: Config):
-        self.input_video_path = input_path
-        self.config = config
-        self.video_capture = None
-        self.classifiers = []
-        self.time_start = None
-        self.frames_count = 0
-        self.frame_current = 0
-        self.duration = None
+        """
+        Setup detector input and configuration.
+        :param str input_path: Path to video in which faces should be detected.
+        :param bragi.Config config: Configuration.
+        """
+
+        self.input_video_path = input_path  # Path to the video
+        self.config = config                # Config's instance
+        self.video_capture = None           # cv2.VideoCapture's instance
+        self.classifiers = []               # List of cv2.CascadeClassifiers' instances
+        self.time_start = None              # Time when detection started
+        self.frames_count = 0               # Total number of frames in video
+        self.frame_current = 0              # Current frame number
+        self.duration = None                # Expected duration as string
 
     def __enter__(self):
         if not os.path.isfile(self.input_video_path):
             raise FileNotFoundError("Input video doesn't exist.")
         
         self.video_capture = cv2.VideoCapture(self.input_video_path)
-        self.frames_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.frames_count = int(self.video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
         for classifier_path in self.config.detection.classifiers:
             self.classifiers.append(
@@ -45,7 +65,7 @@ class Detector:
         while self.video_capture.isOpened():
 
             # Get one frame
-            ret, frame = video.read()
+            ret, frame = self.video_capture.read()
 
             # On error
             if not ret:
@@ -53,30 +73,35 @@ class Detector:
 
             # Skip some frames to process it faster
             self.frame_current += 1
-            if frame_current % self.config.detection.skip_frames != 0:
+            if self.frame_current % self.config.detection.skip_frames != 0:
                 continue
 
             # Calculate duration time
             if self.frame_current % 500 == 0:
-                elapsed = time.time() - timer_start
+                elapsed = time.time() - self.time_start
                 self.timer_start = time.time()
                 self.duration = datetime.timedelta(seconds=math.floor((self.frames_count - self.frame_current) / 500 * elapsed))
 
-            # Detect faces in grayscale image
+            # cv2 detection works only on grayscale image
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             all_faces = []
+
+            # Detect with all classifiers
             for classifier in self.classifiers:
                 faces = classifier.detectMultiScale(gray, scaleFactor=self.config.detection.scale_factor, minNeighbors=self.config.detection.min_neighbors)
-                if len(faces) != 0:        
-                    for face in faces:
-                        x,y,w,h = face
-                        all_faces.append(
-                            frame[y:y+h, x:x+w]
-                        )
+                for face in faces:
+                    x,y,w,h = face
+
+                    # Ignore small faces
+                    if w < self.config.detection.output.min_width or h < self.config.detection.output.min_height:
+                        continue
+
+                    all_faces.append(
+                        frame[y:y+h, x:x+w]
+                    )
 
             if len(all_faces) != 0:
                 yield all_faces
-
 
 
 # ----
