5c7d57b6978139b2a0e8070a354fc10d84192a1b
malja <github@malcak.cz>
2018-12-29 23:53:44 +0100

    Removed helpers.py file

--- bragi/__init__.py
+++ bragi/__init__.py
@@ -1,3 +1,5 @@
 from bragi.config import Config
 from bragi.components import Detector
 from bragi.image import Image
+from bragi.constants import Constants
+from bragi.person import Person
--- bragi/components/__init__.py
+++ bragi/components/__init__.py
@@ -1,2 +1,3 @@
 from bragi.components.detector import Detector
 from bragi.components.recognizer import Recognizer
+from bragi.components.face_recognition_model import FaceRecognitionModel
